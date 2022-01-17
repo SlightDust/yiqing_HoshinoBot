d5eb88d8254a403315bffc7e8544e423b0de5b1b
SlightDust <zzc_200014@163.com>
2022-01-18 02:24:22 +0800

    增加风险地区查询功能，图片自动调整大小

--- yiqing.py
+++ yiqing.py
@@ -1,37 +1,41 @@
 import json
 from sys import path_importer_cache
 import hoshino
-from hoshino import Service,priv
+from hoshino import Service, priv
 from hoshino import aiorequests
 from hoshino.util import FreqLimiter
-from PIL import Image,ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont
 import io
 import os
 import base64
-
 
 flmt = FreqLimiter(5)
 url = "https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5"  # 腾讯api
 
 sv = Service(
-    name = '疫情数据',  #功能名
-    use_priv = priv.NORMAL, #使用权限   
-    manage_priv = priv.ADMIN, #管理权限
-    visible = True, #False隐藏
-    enable_on_default = True, #是否默认启用
-    )
-    
-    
+    name='疫情数据',  # 功能名
+    use_priv=priv.NORMAL,  # 使用权限
+    manage_priv=priv.ADMIN,  # 管理权限
+    visible=True,  # False隐藏
+    enable_on_default=True,  # 是否默认启用
+)
+
+
 # ============================================ #
 
 
 async def get_yiqing_data(area: str) -> str:
     # 应该不会有人闲到写全称吧
-    if area == "内蒙古自治区": area = "内蒙古"
-    elif area == "宁夏回族自治区": area = "宁夏"
-    elif area == "新疆维吾尔自治区": area = "新疆"
-    elif area == "西藏自治区": area = "西藏"
-    elif area == "广西壮族自治区": area = "广西"
+    if area == "内蒙古自治区":
+        area = "内蒙古"
+    elif area == "宁夏回族自治区":
+        area = "宁夏"
+    elif area == "新疆维吾尔自治区":
+        area = "新疆"
+    elif area == "西藏自治区":
+        area = "西藏"
+    elif area == "广西壮族自治区":
+        area = "广西"
     type_ = ""  # 标记是省还是市
     result = {}
     msg = ""
@@ -82,8 +86,13 @@ async def get_yiqing_data(area: str) -> str:
                     if city['name'] == area:
                         result = city
                         type_ = "(市)"
+    if area in ['北京', '天津', '重庆', '上海']:
+        type_ = "(直辖市)"
+    elif area in ['香港', '澳门']:
+        type_ = "(特别行政区)"
     msg += f"{result['name']}{type_}疫情：\n"
-    msg += f"现存确诊：{result['total']['nowConfirm']}" + (f"(+{result['today']['confirm']})" if result['today']['confirm'] > 0 else "")
+    msg += f"现存确诊：{result['total']['nowConfirm']}" + (
+        f"(+{result['today']['confirm']})" if result['today']['confirm'] > 0 else "")
     msg += "\n"
     try:
         msg += f"现存疑似：{result['total']['suspect']}\n"
@@ -93,37 +102,62 @@ async def get_yiqing_data(area: str) -> str:
     try:
         msg += f"累计死亡：{result['total']['dead']} ({result['total']['deadRate']}%)\n"
     except:
-        msg += f"累计死亡：{result['total']['dead']} ({(result['total']['dead']/result['total']['confirm']*100):.2f}%)\n"
+        msg += f"累计死亡：{result['total']['dead']} ({(result['total']['dead'] / result['total']['confirm'] * 100):.2f}%)\n"
     try:
         msg += f"累计治愈：{result['total']['heal']} ({result['total']['healRate']}%)\n"
     except:
-        msg += f"累计治愈：{result['total']['heal']} ({(result['total']['heal']/result['total']['confirm']*100):.2f}%)\n"
+        msg += f"累计治愈：{result['total']['heal']} ({(result['total']['heal'] / result['total']['confirm'] * 100):.2f}%)\n"
     try:
         msg += f"风险等级：{result['total']['grade']}\n" if type_ == "(市)" else ""
     except:
         msg += "无法获取风险等级信息\n"
-    msg += f"当前地区信息今日已更新\n最后更新时间：\n{data['lastUpdateTime']}" if result['today']['isUpdated'] else "！当前地区信息今日无更新"
-    return msg
+    msg += f"当前地区信息今日已更新\n最后更新时间：\n{data['lastUpdateTime']}\n" if result['today']['isUpdated'] else "！当前地区信息今日无更新\n"
 
+    url_risk_area = "https://wechat.wecity.qq.com/api/PneumoniaTravelNoAuth/queryAllRiskLevel"
+    payload_json = {"args": {"req": {}}, "service": "PneumoniaTravelNoAuth", "func": "queryAllRiskLevel",
+                    "context": {"userId": "a"}}
+    risk_area_data = await aiorequests.post(url=url_risk_area, json=payload_json)
+    risk_area_data = await risk_area_data.json()
+    risk_area_data = risk_area_data['args']['rsp']
+    mediumRiskAreaList = risk_area_data['mediumRiskAreaList']
+    highRiskAreaList = risk_area_data['highRiskAreaList']
+
+    if "风险等级：" not in msg: # 没有获取到风险等级
+        return msg
+
+    msg += '中风险地区：\n'
+    for i in mediumRiskAreaList:
+        for j in i['list']:
+            if j['cityName'] in [area, area + "市"]:
+                msg += f"  {j['areaName']} {j['communityName']}\n"
+    msg += '高风险地区：\n'
+    for i in highRiskAreaList:
+        for j in i['list']:
+            if j['cityName'] in [area, area + "市"]:
+                msg += f"  {j['areaName']} {j['communityName']}\n"
+
+    return msg
 
 
 def image_draw(msg):
     fontpath = font_path = os.path.join(os.path.dirname(__file__), 'simhei.ttf')
-    img = Image.new("RGB",(210,200),(255,255,255))
-    draw = ImageDraw.Draw(img)
     font1 = ImageFont.truetype(fontpath, 16)
+    width, height = font1.getsize_multiline(msg.strip())
+    img = Image.new("RGB", (width + 20, height + 20), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
     draw.text((10, 10), msg, fill=(0, 0, 0), font=font1)
     b_io = io.BytesIO()
-    img.save(b_io, format = "JPEG")
+    img.save(b_io, format="JPEG")
     base64_str = 'base64://' + base64.b64encode(b_io.getvalue()).decode()
     return base64_str
 
+
 @sv.on_suffix("疫情")
 @sv.on_prefix("疫情")
-async def yiqing(bot,ev):
-    #冷却器检查
+async def yiqing(bot, ev):
+    # 冷却器检查
     if not flmt.check(ev['user_id']):
-        await bot.send(ev,f"查询冷却中，请{flmt.left_time(ev['user_id'])}秒后再试~",at_sender=True)
+        await bot.send(ev, f"查询冷却中，请{flmt.left_time(ev['user_id']):.2f}秒后再试~", at_sender=True)
         return
     area = ev.message.extract_plain_text()
     try:
@@ -134,14 +168,16 @@ async def yiqing(bot,ev):
             msg = "无法查询该地区疫情"
         else:
             msg = f"查询{area}数据失败：{e}"
-        flmt.start_cd(ev['user_id'],3)
-    if len(msg)<30:
-        await bot.send(ev,msg)
+            repr(e)
+        flmt.start_cd(ev['user_id'], 3)
+    if len(msg) < 30:
+        await bot.send(ev, msg)
     else:
         pic = image_draw(msg)
         await bot.send(ev, f'[CQ:image,file={pic}]')
 
+
 @sv.on_fullmatch('疫情帮助')
-async def yiqing_help(bot,ev):
-    help_msg = "输入xx[省市]疫情，获取xx地区的疫情信息。xx只可输入一级或二级行政区名。省代表一级行政区（包括直辖市、省、自治区、特别行政区），市代表地级行政区（包括地级市、地区、自治州、盟）"
-    await bot.send(ev,help_msg)
+async def yiqing_help(bot, ev):
+    help_msg = "输入xx[省市]疫情，获取xx地区的疫情信息。xx只可输入一级或二级行政区名。"
+    await bot.send(ev, help_msg)
